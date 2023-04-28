f51303120e123224bda75b50af8e28c6cc0398a6
Adrian Hieber <adrian.hieber@gmail.com>
2023-04-28 18:32:56 +0200

    draw function

---
+++ P1/a1.py
@@ -0,0 +1,49 @@
+# author Adrian Hieber
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#funtions
+f1 = lambda x, y: 5 * (x ** 2) - 6 * x * y + 5 * (y ** 2)
+f2 = lambda x, y: 100 * (y - (x ** 2)) ** 2 + (1 - x) ** 2
+
+#init
+fig = plt.figure()
+
+ax_f1_3d = fig.add_subplot(2,3,1,projection='3d')
+ax_f1_2d_x = fig.add_subplot(2,3,2)
+ax_f1_2d_y = fig.add_subplot(2,3,3)
+
+ax_f2_3d = fig.add_subplot(2,3,4,projection='3d')
+ax_f2_2d_x = fig.add_subplot(2,3,5)
+ax_f2_2d_y = fig.add_subplot(2,3,6)
+
+# Make data
+x = np.linspace(-10, 10)
+y = np.linspace(-10, 10)
+x_mg, y_mg = np.meshgrid(x,y)
+z_f1 = f1(x_mg,y_mg)
+z_f2 = f2(x_mg,y_mg)
+
+# Plot the surface
+ax_f1_3d.plot_surface(x_mg, y_mg, z_f1)
+ax_f1_3d.set(xlabel="x", ylabel="y", zlabel="f1(x,y)", title="F1 3d")
+
+ax_f1_2d_x.plot(x,f1(x,y))
+ax_f1_2d_x.set(xlabel="x", ylabel="f1(x,y)", title="F1 2d x-axis")
+
+ax_f1_2d_y.plot(y,f1(x,y))
+ax_f1_2d_y.set(xlabel="y", ylabel="f1(x,y)", title="F1 2d y-axis")
+
+ax_f2_3d.plot_surface(x_mg, y_mg, z_f2)
+ax_f2_3d.set(xlabel="x", ylabel="y", zlabel="f1(x,y)", title="F2 3d")
+
+ax_f2_2d_x.plot(x,f2(x,y))
+ax_f2_2d_x.set(xlabel="x", ylabel="f2(x,y)", title="F2 2d x-axis")
+
+ax_f2_2d_y.plot(y,f2(x,y))
+ax_f2_2d_y.set(xlabel="y", ylabel="f2(x,y)", title="F2 2d y-axis")
+
+fig.tight_layout()
+
+plt.show()
